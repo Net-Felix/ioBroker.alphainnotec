e2fa650c838cb58ee029649e804834b4eecc920d
Felix Amler <felix@amler.eu>
2022-07-26 09:57:42 +0200

    initial release

--- main.js
+++ main.js
@@ -9,7 +9,9 @@
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
-// const fs = require("fs");
+const WebSocketClient = require("websocket").client;
+const xml2js = require("xml2js");
+
 
 class Alphainnotec extends utils.Adapter {
 
@@ -21,11 +23,151 @@ class Alphainnotec extends utils.Adapter {
 			...options,
 			name: "alphainnotec",
 		});
+
+		this.ws = new WebSocketClient();
+		this.connection;
+		this.statusinterval;
+		this.pollinterval;
+		const parseroptions = {
+			explicitArray: false,
+			mergeAttrs: true
+		};
+		this.xmlparser = new xml2js.Parser(parseroptions);
+
 		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
+		// this.on("stateChange", this.onStateChange.bind(this));
 		// this.on("objectChange", this.onObjectChange.bind(this));
 		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
+	}
+
+	/**
+     * This will setup the websocket connection to the Heatpump.
+     */
+	wsConnect() {
+		// bind to events
+		this.ws.on("connect", this.wsHandleConnection.bind(this));
+		this.ws.on("connectFailed", this.wsHandleError.bind(this));
+		this.log.debug("Connecting to heatpump");
+		this.ws.connect("ws://" + this.config.ipaddress + ":" + this.config.port, "Lux_WS");
+	}
+
+	// Reconnect to websoccet
+	wsReconnect(error) {
+		this.log.error("Connect failed: " + error.toString());
+		this.log.info("Trying to reconnect...");
+		this.wsConnect();
+	}
+
+	/**
+     * This handles errors on connection
+     * @param {*} error
+     */
+	async wsHandleError(error) {
+		this.log.error("got an error: " + error.toString());
+	}
+
+	async wsOnClose() {
+		// nothing here yet
+	}
+
+	/**
+     * This handles Websoccet connection
+     * @param {*} connection
+     */
+	async wsHandleConnection(connection) {
+		if (connection.connected) {
+			this.connection = connection;
+			this.log.info("Successfully connected to heatpump");
+
+			// handle connection events
+			this.connection.on("error", this.wsHandleError.bind(this));
+			this.connection.on("close", this.wsOnClose.bind(this));
+			this.connection.on("message", this.wsParseMessage.bind(this));
+		}
+	}
+
+	async wsPollData() {
+		if (this.connection && this.connection.connected) {
+			this.connection.send("LOGIN;" + this.config.password);
+		}
+	}
+
+	async wsParseMessage(message) {
+		this.xmlparser.parseString("" + message.utf8Data, this.wsHandleMessage.bind(this));
+	}
+
+	async wsHandleMessage(error, message) {
+		// check if we got navigation
+		if (message.Navigation) {
+			message.Navigation.item.item.forEach(this.wsGetItems.bind(this));
+		} else {
+			this.wsProcessResponse(message.Content.name, message.Content.item)
+		}
+	}
+
+	async wsGetItems(section) {
+		this.connection.send("GET;" + section.id);
+	}
+
+	wsGetBool(str) {
+		const booldefs = {
+			"ein": true,
+			"on": true,
+			"an": true,
+			"aus": false,
+			"off": false,
+		};
+		return booldefs[str.toLowerCase()];
+	}
+
+	async wsProcessResponse(topic, items) {
+		for (const [key, value] of Object.entries(items)) {
+			if (value.name) {
+				let type = "mixed";
+				let val = value.value;
+				const name = value.name.replace(/\./g, "");
+				let state = topic.trim() + "." + name.trim();
+
+				// check if we got a float with a unit
+				if ((/^-?\s*[\d\.]+\s*[^\d]+$/.test(val)) && (!/^\d+:/.test(val))) {
+					const matches = val.match(/^(-?\s*[\d\.]+)\s*([^\d]+)$/);
+					val = parseFloat(matches[1]);
+					type = "number";
+				} else if (this.wsGetBool(value.value) !== undefined) {
+					val = this.wsGetBool(value.value);
+					type = "boolean";
+				}
+
+				// check if exists with different type
+				const check = await this.getObjectAsync(state);
+				if (check && check.common.type !== type) {
+					state = topic.trim() + "._" + name.trim();
+				}
+
+				await this.setObjectNotExistsAsync(state, {
+					type: "state",
+					common: {
+						name: value.name,
+						type: type,
+						role: "state",
+						read: true,
+						write: false,
+					},
+					native: {},
+				});
+				this.setStateAsync(state, { val: val, ack: true });
+			}
+		}
+	}
+
+	async wsCheckStatus() {
+		if (this.connection && this.connection.connected) {
+			this.setState("info.connection", { val: true, ack: true });
+		} else {
+			this.setState("info.connection", { val: false, ack: true });
+			this.wsReconnect("Lost Connection");
+		}
 	}
 
 	/**
@@ -37,55 +179,14 @@ class Alphainnotec extends utils.Adapter {
 		// Reset the connection indicator during startup
 		this.setState("info.connection", false, true);
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.wsConnect();
+		let interval = this.config.polltime;
+		if (interval < 5) {
+			interval = 5;
+		}
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
-
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
-
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		this.pollinterval = setInterval(this.wsPollData.bind(this), interval * 1000);
+		this.statusinterval = setInterval(this.wsCheckStatus.bind(this), 1000);
 	}
 
 	/**
@@ -94,67 +195,14 @@ class Alphainnotec extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
+			clearInterval(this.pollinterval);
+			clearInterval(this.statusinterval);
 
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
 
 }
 
